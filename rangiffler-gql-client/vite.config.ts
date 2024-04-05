ae6294fff1235139805612d6b534fe426304766e
Alexander <a.kireev.qa@gmail.com>
2024-04-05 22:24:48 +0300

    feat: поднять backend в докере, создать конфиг для фронта в докере

--- rangiffler-gql-client/vite.config.ts
+++ rangiffler-gql-client/vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig(({ mode }) => {
     plugins: [react()],
     server: {
       host: process.env.VITE_FRONT_HOST,
-      port: 3001,
+      port: process.env.VITE_FRONT_PORT,
     },
     build: {
       outDir: 'build'
