96ce5f556317b5f98aeee4c860ca6f747633e490
Alexander <a.kireev.qa@gmail.com>
2024-04-07 22:17:54 +0300

    feat: добавить GraphqlExceptionHandler

---
+++ rangiffler-gateway/src/main/java/com/onehundredtwentyninth/rangiffler/service/GraphqlExceptionHandler.java
@@ -0,0 +1,42 @@
+package com.onehundredtwentyninth.rangiffler.service;
+
+import graphql.GraphQLError;
+import graphql.GraphqlErrorBuilder;
+import graphql.schema.DataFetchingEnvironment;
+import java.util.NoSuchElementException;
+import javax.annotation.Nonnull;
+import org.springframework.graphql.execution.DataFetcherExceptionResolverAdapter;
+import org.springframework.graphql.execution.ErrorType;
+import org.springframework.stereotype.Component;
+
+@Component
+public class GraphqlExceptionHandler extends DataFetcherExceptionResolverAdapter {
+
+  @Override
+  protected GraphQLError resolveToSingleError(@Nonnull Throwable ex, @Nonnull DataFetchingEnvironment env) {
+    if (ex instanceof IllegalArgumentException || ex instanceof IllegalStateException) {
+      return GraphqlErrorBuilder.newError()
+          .errorType(ErrorType.BAD_REQUEST)
+          .message(ex.getMessage())
+          .path(env.getExecutionStepInfo().getPath())
+          .location(env.getField().getSourceLocation())
+          .build();
+    }
+
+    if (ex instanceof NoSuchElementException) {
+      return GraphqlErrorBuilder.newError()
+          .errorType(ErrorType.NOT_FOUND)
+          .message(ex.getMessage())
+          .path(env.getExecutionStepInfo().getPath())
+          .location(env.getField().getSourceLocation())
+          .build();
+    }
+
+    return GraphqlErrorBuilder.newError()
+        .errorType(ErrorType.INTERNAL_ERROR)
+        .message(ex.getMessage())
+        .path(env.getExecutionStepInfo().getPath())
+        .location(env.getField().getSourceLocation())
+        .build();
+  }
+}
