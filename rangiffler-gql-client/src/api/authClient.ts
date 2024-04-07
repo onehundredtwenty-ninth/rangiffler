88534b26d0e6776298c0113f1a1160458ee85f0b
Alexander <a.kireev.qa@gmail.com>
2024-04-07 19:36:02 +0300

    fix: передавать параметры в url при запросе на /oauth2/token

--- rangiffler-gql-client/src/api/authClient.ts
+++ rangiffler-gql-client/src/api/authClient.ts
@@ -3,14 +3,14 @@ import {Buffer} from "buffer";
 
 export const authClient = {
     getToken: async(url: string, data: URLSearchParams) => {
-        const response = await fetch(`${BASE_URL}/${url}`, {
+        const params = "code=" + data.get("code") + "&redirect_uri=" + data.get("redirect_uri") + "&code_verifier=" + data.get("code_verifier") + "&grant_type=" + data.get("grant_type") + "&client_id=" + data.get("client_id")
+        const response = await fetch(`${BASE_URL}/${url}?${params}`, {
             method: "POST",
             credentials: "include",
             headers: {
                 "Content-type": "application/x-www-form-urlencoded",
                 "Authorization": `Basic ${Buffer.from("client:secret").toString("base64")}`,
-            },
-            body: data.toString()
+            }
         });
         if (!response.ok) {
             throw new Error("Failed loading data");
