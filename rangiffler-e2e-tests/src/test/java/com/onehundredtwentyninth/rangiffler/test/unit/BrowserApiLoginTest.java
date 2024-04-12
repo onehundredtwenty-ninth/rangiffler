b9883d4c209af3ee962036ce63e40fc4c6c8a5e7
Alexander <a.kireev.qa@gmail.com>
2024-04-12 14:42:12 +0300

    feat: добавить BrowserLoginExtension

---
+++ rangiffler-e2e-tests/src/test/java/com/onehundredtwentyninth/rangiffler/test/unit/BrowserApiLoginTest.java
@@ -0,0 +1,24 @@
+package com.onehundredtwentyninth.rangiffler.test.unit;
+
+import com.onehundredtwentyninth.rangiffler.jupiter.ApiLogin;
+import com.onehundredtwentyninth.rangiffler.jupiter.ApiLoginExtension;
+import com.onehundredtwentyninth.rangiffler.jupiter.BrowserLoginExtension;
+import com.onehundredtwentyninth.rangiffler.jupiter.CreateUserExtension;
+import com.onehundredtwentyninth.rangiffler.jupiter.Token;
+import com.onehundredtwentyninth.rangiffler.page.MyTravelsPage;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+@ExtendWith({CreateUserExtension.class, ApiLoginExtension.class, BrowserLoginExtension.class})
+class BrowserApiLoginTest {
+
+  private final MyTravelsPage myTravelsPage = new MyTravelsPage();
+
+  @ApiLogin(username = "bee", password = "123")
+  @Test
+  void apiLoginForRandomCreatedUserTest(@Token String token) {
+    Assertions.assertNotNull(token);
+    myTravelsPage.travelsMapHeaderShouldBeVisible();
+  }
+}
