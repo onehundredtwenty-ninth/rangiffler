816b53ad29e5b6d2dda3d3ab4f25c193cfce11f7
Alexander <a.kireev.qa@gmail.com>
2024-04-13 20:52:18 +0300

    test: добавить классы с константами под эпики, фичи, сьюты

--- rangiffler-e2e-tests/src/test/java/com/onehundredtwentyninth/rangiffler/test/grpc/GeoServiceTest.java
+++ rangiffler-e2e-tests/src/test/java/com/onehundredtwentyninth/rangiffler/test/grpc/GeoServiceTest.java
@@ -2,15 +2,27 @@ package com.onehundredtwentyninth.rangiffler.test.grpc;
 
 import com.google.protobuf.Empty;
 import com.onehundredtwentyninth.rangiffler.config.Config;
+import com.onehundredtwentyninth.rangiffler.constant.Epics;
+import com.onehundredtwentyninth.rangiffler.constant.Features;
+import com.onehundredtwentyninth.rangiffler.constant.Layers;
+import com.onehundredtwentyninth.rangiffler.constant.Suites;
 import com.onehundredtwentyninth.rangiffler.grpc.AllCountriesResponse;
 import com.onehundredtwentyninth.rangiffler.grpc.RangifflerGeoServiceGrpc;
 import com.onehundredtwentyninth.rangiffler.utils.GrpcConsoleInterceptor;
 import io.grpc.ManagedChannelBuilder;
+import io.qameta.allure.Epic;
+import io.qameta.allure.Feature;
 import io.qameta.allure.grpc.AllureGrpc;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Tags;
 import org.junit.jupiter.api.Test;
 
+@Epic(Epics.GEO)
+@Feature(Features.COUNTRY_LIST)
+@Tags({@Tag(Layers.GRPC), @Tag(Suites.SMOKE), @Tag(Epics.GEO), @Tag(Features.COUNTRY_LIST)})
 class GeoServiceTest {
 
   private static final Config CFG = Config.getInstance();
@@ -25,6 +37,7 @@ class GeoServiceTest {
     blockingStub = RangifflerGeoServiceGrpc.newBlockingStub(channel);
   }
 
+  @DisplayName("Получение списка всех стран")
   @Test
   void allCountriesTest() {
     final AllCountriesResponse response = blockingStub.getAllCountries(Empty.getDefaultInstance());
