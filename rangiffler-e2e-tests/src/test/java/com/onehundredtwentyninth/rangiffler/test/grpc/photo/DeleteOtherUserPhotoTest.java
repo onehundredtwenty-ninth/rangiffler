30b63ecec4089fd28a1c4fb0923160fb6aeca4ca
Alexander <a.kireev.qa@gmail.com>
2024-04-16 17:17:42 +0300

    fix: удалить получение фото из БД в deletePhotoTest

--- rangiffler-e2e-tests/src/test/java/com/onehundredtwentyninth/rangiffler/test/grpc/photo/DeleteOtherUserPhotoTest.java
+++ rangiffler-e2e-tests/src/test/java/com/onehundredtwentyninth/rangiffler/test/grpc/photo/DeleteOtherUserPhotoTest.java
@@ -1,15 +1,11 @@
 package com.onehundredtwentyninth.rangiffler.test.grpc.photo;
 
-import com.google.inject.Inject;
 import com.onehundredtwentyninth.rangiffler.assertion.GrpcStatusExceptionAssertions;
 import com.onehundredtwentyninth.rangiffler.constant.Epics;
 import com.onehundredtwentyninth.rangiffler.constant.Features;
 import com.onehundredtwentyninth.rangiffler.constant.JUnitTags;
 import com.onehundredtwentyninth.rangiffler.constant.Layers;
 import com.onehundredtwentyninth.rangiffler.constant.Suites;
-import com.onehundredtwentyninth.rangiffler.db.model.PhotoEntity;
-import com.onehundredtwentyninth.rangiffler.db.repository.CountryRepository;
-import com.onehundredtwentyninth.rangiffler.db.repository.PhotoRepository;
 import com.onehundredtwentyninth.rangiffler.grpc.DeletePhotoRequest;
 import com.onehundredtwentyninth.rangiffler.jupiter.CreateUser;
 import com.onehundredtwentyninth.rangiffler.jupiter.Friend;
@@ -27,11 +23,6 @@ import org.junit.jupiter.api.Test;
 @Tags({@Tag(Layers.GRPC), @Tag(Suites.SMOKE), @Tag(JUnitTags.PHOTOS), @Tag(JUnitTags.DELETE_PHOTO)})
 class DeleteOtherUserPhotoTest extends GrpcPhotoTestBase {
 
-  @Inject
-  private PhotoRepository photoRepository;
-  @Inject
-  private CountryRepository countryRepository;
-
   @DisplayName("Удаление фото другого пользователя")
   @CreateUser(
       friends = @Friend(
@@ -42,14 +33,13 @@ class DeleteOtherUserPhotoTest extends GrpcPhotoTestBase {
   )
   @Test
   void deletePhotoTest(TestUser user) {
-    final PhotoEntity oldPhoto = photoRepository.findByUserId(user.getFriends().get(0).getId()).get(0);
     final DeletePhotoRequest request = DeletePhotoRequest.newBuilder()
         .setUserId(user.getId().toString())
-        .setPhotoId(oldPhoto.getId().toString())
+        .setPhotoId(user.getFriends().get(0).getPhotos().get(0).getId())
         .build();
 
     GrpcStatusExceptionAssertions.assertThatThrownBy(() -> blockingStub.deletePhoto(request))
         .isInstanceOfStatusRuntimeException()
-        .hasPhotoPermissionDeniedMessage(oldPhoto.getId().toString(), user.getId().toString());
+        .hasPhotoPermissionDeniedMessage(user.getFriends().get(0).getPhotos().get(0).getId(), user.getId().toString());
   }
 }
