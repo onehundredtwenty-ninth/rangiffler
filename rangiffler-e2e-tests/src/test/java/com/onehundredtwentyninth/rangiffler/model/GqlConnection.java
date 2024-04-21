0fb0709fac366879064772baef094dfe4f508b2a
Alexander <a.kireev.qa@gmail.com>
2024-04-21 15:28:43 +0300

    refactor: удалить конструктор из GqlConnection

--- rangiffler-e2e-tests/src/test/java/com/onehundredtwentyninth/rangiffler/model/GqlConnection.java
+++ rangiffler-e2e-tests/src/test/java/com/onehundredtwentyninth/rangiffler/model/GqlConnection.java
@@ -1,7 +1,5 @@
 package com.onehundredtwentyninth.rangiffler.model;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import java.util.List;
 import lombok.Getter;
 import lombok.Setter;
@@ -12,10 +10,4 @@ public class GqlConnection<T> extends GqlResponseType {
 
   private List<T> edges;
   private GqlPageInfo pageInfo;
-
-  @JsonCreator(mode = JsonCreator.Mode.PROPERTIES)
-  public GqlConnection(@JsonProperty("edges") List<T> edges, @JsonProperty("pageInfo") GqlPageInfo pageInfo) {
-    this.edges = edges;
-    this.pageInfo = pageInfo;
-  }
 }
