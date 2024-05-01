033b050c27cec0b4e94224088126e2212b3e2651
Alexander <a.kireev.qa@gmail.com>
2024-05-01 16:19:22 +0300

    test: добавить тесты на отображение статистики

--- rangiffler-e2e-tests/src/test/java/com/onehundredtwentyninth/rangiffler/page/MyTravelsPage.java
+++ rangiffler-e2e-tests/src/test/java/com/onehundredtwentyninth/rangiffler/page/MyTravelsPage.java
@@ -7,13 +7,14 @@ import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.SelenideElement;
 import com.onehundredtwentyninth.rangiffler.model.TestPhoto;
 import com.onehundredtwentyninth.rangiffler.page.component.PhotoCardsBar;
+import com.onehundredtwentyninth.rangiffler.page.component.WorldMap;
 
 public class MyTravelsPage extends BasePage<MyTravelsPage> {
 
   private final SelenideElement travelsMapHeader = $x("//h2[text() = 'Travels map']");
   private final SelenideElement onlyMyTravelsButton = $x("//button[text()='Only my travels']");
   private final SelenideElement withFriendsButton = $x("//button[text()='With friends']");
-  private final SelenideElement worldMap = $x("//figure[@class='worldmap__figure-container']");
+  private final WorldMap worldMap = new WorldMap($x("//figure[@class='worldmap__figure-container']"));
   private final SelenideElement addPhotoButton = $x("//button[text()='Add photo']");
   private final PhotoCardsBar photoCardsBar = new PhotoCardsBar($x("//div[contains(@class,'MuiGrid-root MuiGrid-container')]"));
 
@@ -44,6 +45,11 @@ public class MyTravelsPage extends BasePage<MyTravelsPage> {
 
   public MyTravelsPage photosCountShouldBeEqualTo(int expectedCount) {
     photoCardsBar.photosCountShouldBeEqualTo(expectedCount);
+    return this;
+  }
+
+  public MyTravelsPage statisticShouldBePresented(String countryCode, String countryName, int expectedCount) {
+    worldMap.statisticShouldBePresented(countryCode, countryName, expectedCount);
     return this;
   }
 }
